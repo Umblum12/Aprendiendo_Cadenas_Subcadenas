9b7bc69406d1519ba61ed1a74e4732a6d49cefbf
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-30 23:41:54 -0600

    Agregar archivos de proyecto.

---
+++ Aprendiendo_Cadenas_Subcadenas/Program.cs
@@ -0,0 +1,7 @@
+﻿string aprendiendo = "Bienvenidos al mundo de la programación";
+string mayusculas = aprendiendo.ToUpper();
+string minusculas = aprendiendo.ToLower();
+Console.WriteLine(mayusculas);
+Console.WriteLine(minusculas);
+Console.WriteLine(aprendiendo);
+Console.ReadKey(true);
